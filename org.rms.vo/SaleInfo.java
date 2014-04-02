6b75fbac3c1d4f9ea2c31269da2813d3e50ed388
sjh010 <sjh010@naver.com>
2014-04-02 15:50:10 +0900

    Create SaleInfo.java

---
+++ org.rms.vo/SaleInfo.java
@@ -0,0 +1,37 @@
+package org.rms.vo;
+
+public class SaleInfo {
+
+	private String date;
+	private String bulidingId;
+	private String contractType;
+	private int price;
+	private int deposit;
+	private String tenantId;
+	private String hostId;
+	
+	public String getDate() {
+		return date;
+	}
+	public String getBulidingId() {
+		return bulidingId;
+	}
+	public String getContractType() {
+		return contractType;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getDeposit() {
+		return deposit;
+	}
+	public String getTenantId() {
+		return tenantId;
+	}
+	public String getHostId() {
+		return hostId;
+	}
+	
+	
+	
+}
